f99e0f498809a4af18fee57d4fa570d39b162de5
AlexeyKozhevin <allex93@yandex.ru>
2016-05-28 12:08:20 +0300

    Update mdr_for_costs.R

--- mdr_for_costs.R
+++ mdr_for_costs.R
@@ -3,79 +3,97 @@ source("MDR-EFE.R")
 source("distribution.R")
 source("powerplot.R")
 
+#the following parameters are fixed for all experiments
+
 r = 3
 n = 100
 significant_factors = c(2,3,5)
-
-MC <- 100
-costs <- (4:5)*100
-
 model_probabilities <- c(0.05,0.1,0.2)/2
-models <- 3
 ratio <- 0.1
+MC <- 100
+
+#the following parameters take different values. 
+#For each combinations of them we should simulate MC = 100 experiments
+
+costs <- (1:5)*100
+models <- 1:3
+
+#auxiliary variables for names in csv table
 
 signif_name <- paste0("pred",1:r)
 ind_name <- paste0("ind",1:r)
 fix_name <- paste0("fix",1:r)
 
-TMR <- rep(0,5)
-
+#TMR <- rep(0,5)
 
 for (model in models)
 {
   for (cost in costs)
   {
+    
+    #for each combination of variables model and cost create data.frame 
+    #in order to save results of the experiments 
+    
     results <- matrix(0,ncol=10,nrow = MC)
     results <- data.frame(results)
     colnames(results)<-c("model","total_cost","Nstr","tildeN1","tildeN2","powerindkn","powerindun",
                          "powerstr1","powerstr2kn","powerstr2un")
     
-    N_fix <- sstr(cost,ratio,model_probabilities[model])[1]
+    #compute some parameter for MDR which depends on model and cost
+    
+    
+    pr_of_case <- model_probabilities[model]
+    N_fix <- sstr(cost,ratio,pr_of_case)[1]
     N_fix <- 2*(N_fix%/%2)
 
+    #start experiments
     for (rep in 1:MC)
     {   
-      pr_of_case <- model_probabilities[model]
+      #simulate dataset
       
       time <- Sys.time()
       data <- simulated_SNPs(n,N_fix,cost,significant_factors,model,type="comb")
       print(Sys.time() - time)
       
-      #sample ind.known
+      #iMDR, probability of case is known
       
       time <- Sys.time() 
       indep.model.knownp <- mdr.efe(data[[1]],r,type = "indep",prob = pr_of_case,parallel = T)
       print(Sys.time() - time)
       
-      #sample ind.unknown
+      #iMDR, probability of case is unknown
       
       time <- Sys.time() 
       indep.model.unknownp <- mdr.efe(data[[1]],r,type = "indep",parallel = T)
       print(Sys.time() - time)
       
-      #sample str.known, w = 0.1
+      #sMDR, w = 0.1, probability of case is known
       
       time <- Sys.time() 
       fixed.model1 <- mdr.efe(data[[2]],r,type = "fix",prob = pr_of_case,parallel = T)
       print(Sys.time() - time)
       
-      #sample str.known, w = 0
+      #sMDR, w = 0, probability of case is known
       
       time <- Sys.time() 
       fixed.model2.knownp <- mdr.efe(data[[3]],r,type = "fix",prob = pr_of_case,parallel = T)
       print(Sys.time() - time)
       
-      #sample str.unknown, w = 0
+      #sMDR, w = 0, probability of case is unknown
       
       time <- Sys.time() 
       fixed.model2.unknownp <- mdr.efe(data[[3]],r,type = "fix",prob = data[[4]],parallel = T)
       print(Sys.time() - time)
+      
+      #compute results of the experiments
       
       powerindkn <- power(significant_factors,indep.model.knownp)
       powerindun <- power(significant_factors,indep.model.unknownp)
       powerstr1 <- power(significant_factors,fixed.model1)
       powerstr2kn <- power(significant_factors,fixed.model2.knownp)
       powerstr2un <- power(significant_factors,fixed.model2.unknownp)
+      
+      #log is a result which I should know for each experiment 
       
       log <- c(model,cost,N_fix,data[[5]],data[[6]],powerindkn,powerindun,powerstr1,powerstr2kn,powerstr2un)
       
